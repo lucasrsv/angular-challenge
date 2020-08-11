5b9bf3422af56e1acdfc3db6f399f0bc67755778
lucasrsv <lucas.r2737@gmail.com>
2020-08-11 19:17:26 -0300

    sorts comments by dates, implements form validation and configures firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDVpJ-DONSNTfutLzMGZmDWy87TQ0mexVs",
+    authDomain: "angular-project-b7bde.firebaseapp.com",
+    databaseURL: "https://angular-project-b7bde.firebaseio.com",
+    projectId: "angular-project-b7bde",
+    storageBucket: "angular-project-b7bde.appspot.com",
+    messagingSenderId: "1015815825852",
+    appId: "1:1015815825852:web:2143e3649e7dbc586523b9"
+  }
 };
